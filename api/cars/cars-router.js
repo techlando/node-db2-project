b78f9edfae8830650141695b536d05c79783790f
Landon Phillips <landonphillips@Landons-MacBook-Pro.local>
2022-05-18 23:52:44 -0500

    first commit

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,19 @@
 // DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    res.json('foo')
+})
+
+router.get('/', async (req, res, next) => {
+    res.json('cr')
+})
+
+router.post('/', async  (req, res, next) => {
+    res.json('meow')
+})
+
+
+
+module.exports = router;
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,35 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        milage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+
+    },
+    {
+        vin: '2222222222222',
+        make: 'toyota',
+        model: 'corolla',
+        milage: 78321,
+        title: 'salvage',
+        
+
+    },
+    {
+        vin: '3333333333333',
+        make: 'ford',
+        model: 'focus',
+        milage: 10000,
+       
+
+    }
+]
+
+exports.seed = function(knex) {
+    return knex('cars').truncate().then(() => {
+        knex('cars').insert(cars)
+    })
+}
